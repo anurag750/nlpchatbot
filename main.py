d463ebf32c9fa4bcb5a842c15fedda557deb12f4
Anurag <anuragmishra753@gmail.com>
2023-01-16 20:02:54 +0530

    Used transformers library to define a pretrained model

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import warnings
 from ontology_dc8f06af066e4a7880a5938933236037.simple_text import SimpleText
+from transformers import pipeline
 
 from openfabric_pysdk.context import OpenfabricExecutionRay
 from openfabric_pysdk.loader import ConfigClass
@@ -12,8 +13,9 @@ from time import time
 ############################################################
 def config(configuration: ConfigClass):
     # TODO Add code here
-    print(SimpleText)
     pass
+    
+
 
 
 ############################################################
@@ -21,9 +23,12 @@ def config(configuration: ConfigClass):
 ############################################################
 def execute(request: SimpleText, ray: OpenfabricExecutionRay) -> SimpleText:
     output = []
-    for text in request.text:
+    model_name = "distilbert-base-cased-distilled-squad"
+    nlpchatbot = pipeline('question-answering',model=model_name)
+    for text,context in request.text:
         # TODO Add code here
-        response = ''
-        output.append(response)
+        res = nlpchatbot(question=text,context = context)
+        output.append(res[0]['answer'])
 
     return SimpleText(dict(text=output))
+
